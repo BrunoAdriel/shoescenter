04e6bea64d3a6cbdf4186296c79d38f58db184bb
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2023-03-02 22:51:15 -0300

    agrego del navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,18 +5,20 @@ import "./navbar.css";
 function NavBar() {
   return (
     <div className="navbar">
+      <a href="#"><img src="../pictures/sneaker.ico"></img></a>
       <ul>
         <li>
-          <button>Inicio</button>
+        <button class="button-48" role="button"><span class="text">Inicio</span></button>
+        <button class="button-48" role="button"><span class="text">Button 48</span></button>
         </li>
         <li>
-          <button>Productos</button>
+        <button class="button-48" role="button"><span class="text">Productos</span></button>
         </li>
         <li>
-          <button>Ofertas Seleccionadas</button>
+        <button class="button-48" role="button"><span class="text">Ofertas Seleccionadas</span></button>
         </li>
         <li>
-          <button>Sobre Nosotros</button>
+        <button class="button-48" role="button"><span class="text">Sobre Nosotros</span></button>
         </li>
       </ul>
       <CardWidget />
@@ -25,3 +27,8 @@ function NavBar() {
 }
 
 export default NavBar;
+
+
+
+
+
